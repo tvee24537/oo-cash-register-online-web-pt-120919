74eef1e19d2deeb64b8ca537d6c15fc3f75da79c
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-17 20:21:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,43 +1,41 @@
 
 class CashRegister
-
-  attr_accessor :total, :discount, :last_transaction_amount, :items
+  attr_accessor :total, :discount, :quantity, :price
 
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @price = price
+    @quantity = quantity
     @items = []
+    @transactions = []
   end
 
-
-  def add_item(title,price,quantity = 1)
-    if quantity>1
-      i=0
-      while i<quantity
-        @items << title
-        i+=1
-      end
-    else
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+    @transactions << price
+    i = quantity
+    until i == 0 do
       @items << title
+      i -= 1
     end
-    @total += price*quantity
-    @last_transaction_amount = @total
-    @total
   end
 
-  def apply_discount()
-    if @discount > 0
-      @discount = @discount/100.to_f
-      @total = @total - (@total * (@discount))
-      "After the discount, the total comes to $#{@total.to_i}."
-    else
+  def apply_discount
+    if @discount == 0
       "There is no discount to apply."
+    else
+      self.total -= (0.01 * @discount * @total).to_i
+      "After the discount, the total comes to $#{self.total}."
     end
   end
 
-
-  def void_last_transaction()
-    @total -= @last_transaction_amount
+  def items
+    @items
+  end
+  
+  def void_last_transaction #remove the last transaction from the total. 
+	  self.total = @total - @transactions.pop
   end
 
 end
